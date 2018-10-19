21c88bf4c8730f0953d9b6d02caa9cba72516b42
Avahe Kellenberger <avahe.k@gmail.com>
2018-10-19 03:39:35 +0000

    Fixed minor issue to prevent globbing and word splitting.

--- scripts/toggle-touchpad.sh
+++ scripts/toggle-touchpad.sh
@@ -4,7 +4,7 @@ touchpad_id=$(xinput list | grep -i -Pazo '(?<=touchpad).*=([0-9]*)' | grep -i -
 
 status=$(xinput --list-props "$touchpad_id" | grep -i 'device enabled' | grep -Pao '(\d+)(?!.*\d)' | awk '{print $1}')
 
-if [ $status == '1' ]; then
+if [ "$status" == '1' ]; then
     xinput disable "$touchpad_id" 
 else
     xinput enable "$touchpad_id"
