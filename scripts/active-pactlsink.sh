34f9e8ab582ad1003c4e37edced8d7fc0e78c915
Avahe Kellenberger <avahe.k@gmail.com>
2018-10-21 23:31:27 -0400

    Added a script to find the index of the active audio sink with pactl.

---
+++ scripts/active-pactlsink.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+regex="([0-9]+)[^RUNNING|SUSPENDED]*RUNNING"
+
+if [[ $(pactl list sinks short) =~ $regex ]]; then
+    echo ${BASH_REMATCH[1]}
+else
+    echo "Match not found."
+fi
+
