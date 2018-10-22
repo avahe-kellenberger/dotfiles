a4b270cdcd59057aa31495db268637b539e3fce5
Avahe Kellenberger <avahe.k@gmail.com>
2018-10-21 23:36:30 -0400

    Added double quotes to prevent globbing and word splitting.

--- scripts/active-pactlsink.sh
+++ scripts/active-pactlsink.sh
@@ -3,7 +3,7 @@
 regex="([0-9]+)[^RUNNING|SUSPENDED]*RUNNING"
 
 if [[ $(pactl list sinks short) =~ $regex ]]; then
-    echo ${BASH_REMATCH[1]}
+    echo "${BASH_REMATCH[1]}"
 else
     echo "Match not found."
 fi
