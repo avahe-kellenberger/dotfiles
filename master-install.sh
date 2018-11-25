a83e0c845b5f541936ccdb7c308d900d5932d533
Avahe Kellenberger <avahe@protonmail.ch>
2018-11-25 16:21:14 -0500

    Working on a master install script for new arch + openbox installations.

---
+++ master-install.sh
@@ -0,0 +1,38 @@
+sudo pacman -S openbox tint2 rofi tilix openssh lxappearance-obconf vim zsh lolcat xclip nodejs python jq wget pulseaudio pavucontrol compton nitrogen keepass flatpak lightdm lightdm-gtk-greeter firefox teamviewer mps-youtube youtube-dl git curl wmctrl
+
+# private dotfiles
+git clone https://Avahe@bitbucket.org/Avahe/dotfiles.git ~/.keepass
+
+# pb
+git clone https://aur.archlinux.org/pb.git
+cd pb
+makepkg -sric
+cd ..
+rm -r pb
+
+# wmctrl-switch-by-application
+curl -Lo- "https://raw.githubusercontent.com/avahe-kellenberger/wmctrl-switch-by-application/master/install.sh" | sudo bash
+
+# discord
+flatpak install "com.discordapp.Discord"
+
+# dot files
+git clone https://github.com/avahe-kellenberger/dotfiles.git ~/.dotfiles/
+
+# openbox
+ln -s ~/.dotfiles/openbox/ ~/.config/openbox
+ln -s ~/.dotfiles/themes/ ~/.themes
+
+# vim
+ln -s ~/.dotfiles/vim/ ~/.vim/
+echo "export VIMINIT='source $MYVIMRC'
+export MYVIMRC='~/.vim/.vimrc'" >> ~/.zshrc 
+
+# rofi
+ln -s ~/.dotfiles/rofi/ ~/.config/rofi
+
+# zsh
+ln -s ~/.zshrc ~/.dotfiles/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+. ~/.zshrc
