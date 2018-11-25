1d780a42e3620d4e02f2af0e168ca61803db7b49
Avahe Kellenberger <avahe@protonmail.ch>
2018-11-25 18:53:53 -0500

    Updated master install script to be more accurate and complete. Script has past testing.

--- master-install.sh
+++ master-install.sh
@@ -1,4 +1,7 @@
-sudo pacman -S openbox tint2 rofi tilix openssh lxappearance-obconf vim zsh lolcat xclip nodejs python jq wget pulseaudio pavucontrol compton nitrogen keepass flatpak lightdm lightdm-gtk-greeter firefox  mps-youtube youtube-dl git curl wmctrl
+# Notes: Change the shell to zsh prior to running this script.
+# chsh USER -s /usr/bin/zsh
+
+sudo pacman -S openbox tint2 rofi tilix openssh lxappearance-obconf vim zsh lolcat xclip nodejs python jq wget pulseaudio pavucontrol compton nitrogen keepass  lightdm lightdm-gtk-greeter firefox mps-youtube youtube-dl git curl wmctrl xorg-xinit
 
 # private dotfiles
 git clone https://Avahe@bitbucket.org/Avahe/dotfiles.git ~/.keepass
@@ -20,9 +23,6 @@ rm -r teamviewer
 # wmctrl-switch-by-application
 curl -Lo- "https://raw.githubusercontent.com/avahe-kellenberger/wmctrl-switch-by-application/master/install.sh" | sudo bash
 
-# discord
-flatpak install "com.discordapp.Discord"
-
 # dot files
 git clone https://github.com/avahe-kellenberger/dotfiles.git ~/.dotfiles/
 
@@ -42,4 +42,11 @@ ln -s ~/.dotfiles/rofi/ ~/.config/rofi
 ln -s ~/.zshrc ~/.dotfiles/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-. ~/.zshrc
+source ~/.zshrc
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+echo "exec openbox-session"
+
+# Post Install
+# Remove junk applications that are started at the end of ~/.xinitrc
+
