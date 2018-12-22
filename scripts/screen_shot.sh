1c9b8d71074312897eb858ddc4548083aeaf5f25
Avahe Kellenberger <avahe@protonmail.ch>
2018-12-22 01:50:31 -0500

    Changed scrot to maim, as scrot was having issues improperly rendering the rectanglar selection area.

--- scripts/screen_shot.sh
+++ scripts/screen_shot.sh
@@ -1,1 +1,1 @@
-scrot -se 'curl -s -F c=@- https://ptpb.pw/ < $f' | awk '$1 = /url/ { printf $2".png" }' | xclip -sel clipboard
+maim -sou | curl -s -F c=@- https://ptpb.pw/ | awk '$1 = /url/ { printf $2".png" }' | xclip -sel clipboard
