a10356c49035108451f613b736f0e7276937acfc
Avahe Kellenbeger <avahe@protonmail.ch>
2019-05-05 03:50:14 -0400

    Replace i3blocks with polybar.

---
+++ polybar/launch-polybar.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
+        MONITOR=$m polybar --reload example &
+    done
+else
+    polybar --reload example &
+fi
+
