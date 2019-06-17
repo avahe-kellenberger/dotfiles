03dae8ee90b776a29b88c57e084bdfb9c02f904c
Avahe Kellenbeger <avahe@protonmail.ch>
2019-06-17 17:30:20 -0400

    Added pertag patch.

--- dwm/config.h
+++ dwm/config.h
@@ -52,7 +52,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 #include "fibonacci.c"
 /* first entry is default */
