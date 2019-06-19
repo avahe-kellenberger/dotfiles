fdba4fcf841e1aecf2071281832a0d531af16fff
Avahe Kellenbeger <avahe@protonmail.ch>
2019-06-18 22:34:53 -0400

    Added volume to dwmbar.

--- scripts/dwmbar.sh
+++ scripts/dwmbar.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-while true; do xsetroot -name "$(date +"%a %e %b %Y - %I:%M %p %Z")"; sleep 1m; done
+get_status() {
+    echo "Vol: $(get_volume) | $(get_datetime)";
+}
+
+get_volume() {
+    echo "$(pamixer --get-volume-human)";
+}
+
+get_datetime() {
+    echo "$(date +"%a %e %b %Y - %I:%M %p %Z")";
+}
+
+while true
+do
+    xsetroot -name "$(get_status)";
+    sleep 1m;
+done
 
