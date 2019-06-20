6dc47904fae01bf098eb93dd47f3db1fc2a0a39c
Avahe Kellenberger <avahe@protonmail.ch>
2019-06-19 20:44:01 -0400

    Added battery information.

--- scripts/dwmbar.sh
+++ scripts/dwmbar.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Set the overall status
 get_status() {
-    echo "Vol: $(get_volume) | $(get_datetime)";
+    battery_status="$(get_battery_status)";
+    if [[ ! -z battery_status ]]; then
+        battery_status="${battery_status} | "
+    fi
+
+    echo "${battery_status}Vol: $(get_volume) | $(get_datetime)";
 }
 
 get_volume() {
@@ -11,6 +17,29 @@ get_volume() {
 get_datetime() {
     echo "$(date +"%a %e %b %Y - %I:%M %p %Z")";
 }
+
+# Battery
+
+battery="BAT0";
+
+get_battery_status() {
+    battery_status="";
+    charging_status="$(get_charging_status)";
+    if [[ ! -z charging_status ]]; then
+        battery_status="${charging_status} $(get_charge)%"
+    fi
+    echo "$battery_status";
+}
+
+get_charging_status() {
+    echo "$(cat /sys/class/power_supply/$battery/status)"
+}
+
+get_charge() {
+    echo "$(cat /sys/class/power_supply/$battery/capacity)"
+}
+
+# Set the status bar
 
 while true
 do
