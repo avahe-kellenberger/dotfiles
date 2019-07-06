686ec17d9e144a54c857bd2810f2e991915b8900
Avahe Kellenberger <avahe@protonmail.ch>
2019-07-06 09:26:11 -0400

    Added boxdraw patch.

--- st/st.c
+++ st/st.c
@@ -1212,6 +1212,10 @@ tsetchar(Rune u, Glyph *attr, int x, int y)
 	term.dirty[y] = 1;
 	term.line[y][x] = *attr;
 	term.line[y][x].u = u;
+
+	if (isboxdraw(u)) {
+		term.line[y][x].mode |= ATTR_BOXDRAW;
+    }
 }
 
 void
