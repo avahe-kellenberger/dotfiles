49e730a1fb888be1914859bbee6f1687722379fd
Avahe Kellenberger <avahe@protonmail.ch>
2019-07-22 10:16:11 -0400

    Fixed window sizing with gaps.
    Windows now have equal widths with an mfact of 0.5 - verified with
    `xwininfo`.

--- dwm/config.h
+++ dwm/config.h
@@ -50,7 +50,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1880,28 +1880,33 @@ tile(Monitor *m)
 	unsigned int i, n, h, r, oe = enablegaps, ie = enablegaps, mw, my, ty;
 	Client *c;
 
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++)
+        ;
+
+	if (n == 0) {
 		return;
+    }
 
 	if (smartgaps == n) {
 		oe = 0; // outer gaps disabled
 	}
 
-	if (n > m->nmaster)
+	if (n > m->nmaster) {
 		mw = m->nmaster ? (m->ww + m->gappiv*ie) * m->mfact : 0;
-	else
+    } else {
 		mw = m->ww - 2*m->gappov*oe + m->gappiv*ie;
+    }
+
 	for (i = 0, my = ty = m->gappoh*oe, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
 		if (i < m->nmaster) {
 			r = MIN(n, m->nmaster) - i;
 			h = (m->wh - my - m->gappoh*oe - m->gappih*ie * (r - 1)) / r;
-			resize(c, m->wx + m->gappov*oe, m->wy + my, mw - (2*c->bw) - m->gappiv*ie, h - (2*c->bw), 0);
+			resize(c, m->wx + m->gappov*oe, m->wy + my, mw - (2*c->bw) - m->gappiv*ie - m->gappov*oe, h - (2*c->bw), 0);
 			my += HEIGHT(c) + m->gappih*ie;
 		} else {
 			r = n - i;
 			h = (m->wh - ty - m->gappoh*oe - m->gappih*ie * (r - 1)) / r;
-			resize(c, m->wx + mw + m->gappov*oe, m->wy + ty, m->ww - mw - (2*c->bw) - 2*m->gappov*oe, h - (2*c->bw), 0);
+			 resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw) - m->gappov*oe, h - (2*c->bw), 0);
 			ty += HEIGHT(c) + m->gappih*ie;
 		}
     }
