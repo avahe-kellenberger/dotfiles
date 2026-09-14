13bd6eea877e69e1c5b1c0450ba03f96e7dd55ec
Avahe Kellenberger <avahe@protonmail.ch>
2026-09-14 15:43:28 -0400

    Different nvim theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -987,40 +987,54 @@ require('lazy').setup({
     },
   },
 
-  -- { -- You can easily change to a different colorscheme.
-  --   -- Change the name of the colorscheme plugin below, and then
-  --   -- change the command in the config to whatever the name of that colorscheme is.
-  --   --
-  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   'Mofiqul/dracula.nvim',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   config = function()
-  --     ---@diagnostic disable-next-line: missing-fields
-  --     require('dracula').setup {
-  --       styles = {
-  --         comments = { italic = false }, -- Disable italics in comments
-  --       },
-  --     }
-  --
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     vim.cmd.colorscheme 'dracula'
-  --   end,
-  -- },
-
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'Mofiqul/dracula.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('dracula').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-frappe'
+      -- vim.cmd.colorscheme 'dracula'
+    end,
+  },
+
+  {
+    'catppuccin/nvim',
+    priority = 1000,
+    -- config = function() vim.cmd.colorscheme 'catppuccin-frappe' end,
+  },
+
+  {
+    'ribru17/bamboo.nvim',
+    priority = 1000,
+    config = function()
+      require('bamboo').setup {
+        style = 'vulgaris',
+      }
+      -- vim.cmd.colorscheme 'bamboo'
+    end,
+  },
+
+  {
+    'sainnhe/sonokai',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_enable_italic = 0
+      vim.g.sonokai_disable_italic_comment = 1
+      vim.g.sonokai_style = 'atlantis'
+      vim.cmd.colorscheme 'sonokai'
     end,
   },
 
